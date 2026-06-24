3b5867fdc2ffcd1461f75efced122890642d8fae
abokhadega <37779105+abokhadega@users.noreply.github.com>
2026-06-24 12:58:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'azkar-cache-v3';
+const CACHE_NAME = 'azkar-cache-v5';
 const urlsToCache = [
   './index.html',
   './manifest.json',
+  './azkar_obj.json',
   'https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700;800&display=swap'
-  // يمكنك لاحقاً إضافة مسارات صور المصحف هنا إذا أردت تحميلها لتعمل بلا إنترنت
 ];
 
 self.addEventListener('install', event => {
@@ -19,10 +19,27 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // إذا وجد الملف في الكاش (بدون إنترنت)، قم بإرجاعه
         if (response) {
           return response; 
         }
+        // إذا لم يجده، قم بجلبه من الإنترنت
         return fetch(event.request);
       })
+  );
+});
+
+// دالة لمسح الكاش القديم عند تحديث رقم الإصدار
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
